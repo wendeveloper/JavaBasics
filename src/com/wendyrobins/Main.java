10db8ac7c2bc12d1374bc566f3b38f57378cca74
Wendy Robins <wendy@therobins.net>
2022-01-06 13:38:31 -0500

    updated java basics 1/3

--- src/com/wendyrobins/Main.java
+++ src/com/wendyrobins/Main.java
@@ -5,7 +5,14 @@ import javax.naming.Name;
 public class Main {
 
     public static void main(String[] args) {
+        String fName = "Mickey";
+        String lName = "Mouse";
+        String fullName = fName + " " + lName;
+        System.out.println(fName);
+        System.out.println(lName);
+        System.out.println(fullName);
         System.out.println(NameGenerator.generateFullName("John", "Doe"));
+
     }
 
 
